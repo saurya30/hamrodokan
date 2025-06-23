df3922714c5332cf55171b691322e0d216d797e7
saurya30 <khadkasaurya02@gmail.com>
2025-06-23 11:31:17 +0545

    onboarding screen slides

---
+++ lib/features/authentication/screens/onboarding_widgets/onboading_slides.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+import '../../../../utils/constants/image_strings.dart';
+import '../../../../utils/constants/text_strings.dart';
+import '../../controllers/onboarding_controller.dart';
+import 'onboarding_page.dart';
+
+class onboarding_slide extends StatelessWidget {
+  const onboarding_slide({
+    super.key,
+    required this.controller,
+  });
+
+  final OnBoardingController controller;
+
+  @override
+  Widget build(BuildContext context) {
+    return PageView(
+      controller: controller.pageController,
+      onPageChanged: controller.updatePageIndicator,
+      children: [
+        OnBoardingPage(
+          image: TImages.onBoardingImage1,
+          title: TTexts.onBoardingTitle1,
+          subTitle: TTexts.onBoardingSubTitle1,
+        ),
+        OnBoardingPage(
+          image: TImages.onBoardingImage2,
+          title: TTexts.onBoardingTitle2,
+          subTitle: TTexts.onBoardingSubTitle2,
+        ),
+        OnBoardingPage(
+          image: TImages.onBoardingImage3,
+          title: TTexts.onBoardingTitle3,
+          subTitle: TTexts.onBoardingSUbTitle3,
+        ),
+      ],
+    );
+  }
+}
