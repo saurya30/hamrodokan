094d9f3160df6ff113e33e08aecd1015a1d74e3d
Ashmitaa01 <ashmitaacharya011@gmail.com>
2025-06-25 12:48:09 +0545

    appbar

--- lib/features/authentication/screens/home/widget/home.dart
+++ lib/features/authentication/screens/home/widget/home.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:hamrodokan/app.dart';
+import 'package:hamrodokan/common/widgets/appbar/appbar.dart';
 import 'package:hamrodokan/common/widgets/custom_shapes/curved_edges/curved_edges.dart';
+import 'package:hamrodokan/utils/constants/text_strings.dart';
+import 'package:hamrodokan/utils/theme/custom_themes/text_field_theme.dart';
+import 'package:iconsax/iconsax.dart';
 
 import '../../../../../common/widgets/custom_shapes/containers/circular_container.dart';
 import '../../../../../common/widgets/custom_shapes/containers/primary_header_container.dart';
 import '../../../../../common/widgets/custom_shapes/curved_edges/curved_edges_widget.dart';
+import '../../../../../common/widgets/products_cart/cart_menu_icon.dart';
 import '../../../../../utils/constants/colors.dart';
+import 'home_appbar.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -12,13 +19,17 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(),
       body: SingleChildScrollView(
         child: Column(
           children: [
             TPrimaryHeaderContainer(
-                child: Container(),
+              child: Column(
+                children: [
+                  THomeAppBar(),
+                ],
+              ),
             ),
-
           ],
         ),
       ),
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
+
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
@@ -7,21 +7,23 @@ import 'package:get_storage/get_storage.dart';
 import 'app.dart';
 import 'data/repositories/authentication/authentication_repository.dart';
 
-
-Future<void> main(dynamic DefaultFirebaseOptions) async{
-  ///widgets binding
-  final WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
-
-  ///GetX Local Storage
-  await GetStorage.init();
-
-  ///await splash until other items load
-  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
-
-  ///initialize firebase
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform).then(
-      (FirebaseApp value) => Get.put(AuthenticationRepository()),
-  );
-
+// Future<void> main(dynamic DefaultFirebaseOptions) async {
+//   ///widgets binding
+//   final WidgetsBinding widgetsBinding = WidgetsFlutterBinding
+//       .ensureInitialized();
+//
+//   ///GetX Local Storage
+//   await GetStorage.init();
+//
+//   ///await splash until other items load
+//   FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+//
+//   ///initialize firebase
+//   // await Firebase
+//   //     .initializeApp(options: DefaultFirebaseOptions.currentPlatform)
+//   //     .then(
+//   //       (FirebaseApp value) => Get.put(AuthenticationRepository()),
+//   // );
+void main() {
   runApp(const App());
 }
