991d5e1902d18a73ff87ba4fc8bab420b68c1490
saurya30 <khadkasaurya02@gmail.com>
2025-06-26 19:12:30 +0545

    carousel slider

--- lib/features/authentication/screens/home/widget/home.dart
+++ lib/features/authentication/screens/home/widget/home.dart
@@ -1,23 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:hamrodokan/app.dart';
-import 'package:hamrodokan/common/widgets/appbar/appbar.dart';
-import 'package:hamrodokan/common/widgets/custom_shapes/curved_edges/curved_edges.dart';
+import 'package:hamrodokan/features/authentication/screens/home/widget/promo_slider.dart';
 import 'package:hamrodokan/utils/constants/image_strings.dart';
-import 'package:hamrodokan/utils/constants/sizes.dart';
-import 'package:hamrodokan/utils/constants/text_strings.dart';
-import 'package:hamrodokan/utils/device/device_utility.dart';
-import 'package:hamrodokan/utils/helpers/helper_functions.dart';
-import 'package:hamrodokan/utils/theme/custom_themes/text_field_theme.dart';
-import 'package:iconsax/iconsax.dart';
 
-import '../../../../../common/widgets/custom_shapes/containers/circular_container.dart';
+import 'package:hamrodokan/utils/constants/sizes.dart';
+
 import '../../../../../common/widgets/custom_shapes/containers/primary_header_container.dart';
 import '../../../../../common/widgets/custom_shapes/containers/search_container.dart';
-import '../../../../../common/widgets/custom_shapes/curved_edges/curved_edges_widget.dart';
-import '../../../../../common/widgets/image_text_widgets/vertical_image_text.dart';
-import '../../../../../common/widgets/products_cart/cart_menu_icon.dart';
+
 import '../../../../../common/widgets/texts/section_heading.dart';
-import '../../../../../utils/constants/colors.dart';
 import 'home_appbar.dart';
 import 'home_categories.dart';
 
@@ -48,10 +38,10 @@ class HomeScreen extends StatelessWidget {
                     child: Column(
                       children: [
                         ///Heading
-                       const TSectionHeading(
+                        const TSectionHeading(
                           title: 'Popular Categories',
                           showActionButton: false,
-                         textColor: Colors.white,
+                          textColor: Colors.white,
                         ),
                         const SizedBox(height: TSizes.spaceBtwItems),
 
@@ -63,14 +53,23 @@ class HomeScreen extends StatelessWidget {
                 ],
               ),
             ),
+
+            ///body
+            Padding(
+                padding: EdgeInsets.all(TSizes.defaultSpace),
+              child: Column(
+                children: [
+
+                  ///promo slider
+                  TPromoSlider(banners: [TImages.promoBanner1,TImages.promoBanner2,TImages.promoBanner3]),
+
+
+                ],
+              ),
+            ),
           ],
         ),
       ),
     );
   }
 }
-
-
-
-
-
